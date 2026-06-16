eeeb9cbcb27f6032fe06068ef9ef6228799f56cd
Shriram Dani <shriramdani132004@gmail.com>
2026-06-16 06:20:41 +0530

    Fix timezone configuration

--- backend/src/main/java/com/support/backend/BackendApplication.java
+++ backend/src/main/java/com/support/backend/BackendApplication.java
@@ -1,5 +1,7 @@
 package com.support.backend;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BackendApplication {
 
 	public static void main(String[] args) {
+
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
+
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
